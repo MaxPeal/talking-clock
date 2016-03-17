dd47b496a3149ac756a890cb068da6bac1022f0f
Michael Gilbertson <mgg9xv@virginia.edu>
2016-03-16 21:18:36 -0400

    gh pages first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# verbal-clock
+A verbal clock that I was inspired to recreate as a web page.
+
+Here's the [imgur link](http://imgur.com/ZkhdjTs) that inspired me. 
---
+++ app.js
@@ -0,0 +1,138 @@
+(function(){
+    'use strict';
+
+    var Word = function(text){
+        this.text = text;
+        this.status = false;
+    };
+
+    var data = {
+        date: new Date(),
+        words: {
+            it: new Word("It"),
+            is: new Word("is"),
+            fiveMin: new Word("five"),
+            tenMin: new Word("ten"),
+            quarter: new Word("quarter"),
+            half: new Word("half"),
+            to: new Word("to"),
+            past: new Word("past"),
+            one: new Word("one"),
+            two: new Word("two"),
+            three: new Word("three"),
+            four: new Word("four"),
+            five: new Word("five"),
+            six: new Word("six"),
+            seven: new Word("seven"),
+            eight: new Word("eight"),
+            nine: new Word("nine"),
+            ten: new Word("ten"),
+            eleven: new Word("eleven"),
+            twelve: new Word("twelve"),
+            oclock: new Word("o'clock"),
+            am: new Word("AM"),
+            pm: new Word("PM")
+        }
+    };
+
+    var controller = {
+        init: function(){
+            this.setClock();
+            view.init();
+        },
+        setClock: function(){
+            for (var word in data.words) {
+                data.words[word].status = false;
+            }
+            this.setVerbage();
+            this.setMinutes();
+            this.setHours();
+            this.setMeridian();
+        },
+        setVerbage: function(){
+            var minutes = data.date.getMinutes();
+            data.words.it.status = true;
+            data.words.is.status = true;
+            data.words.oclock.status= true;
+            if ( (minutes >= 3) && ( minutes <= 38)){
+                data.words.past.status = true;
+            } else if ( (minutes >= 39) && (minutes <= 57)){
+                data.words.to.status = true;
+            }
+        },
+        setMinutes: function(number){
+            var minutes = data.date.getMinutes();
+            if ( ((3 <= minutes) && (minutes <= 7)) || ((minutes >= 53) && (minutes <= 57))) {
+                data.words.fiveMin.status = true;
+            } else if (((8 <= minutes) && (minutes <= 12)) || ((minutes >= 48) && (minutes <= 52))) {
+                data.words.tenMin.status = true;
+            } else if (((13 <= minutes) && (minutes <= 23)) || ((minutes >= 39) && (minutes <= 47))) {
+                data.words.quarter.status = true;
+            } else if ((24 <= minutes) && (minutes <= 38)) {
+                data.words.half.status = true;
+            }
+        },
+        setHours: function(){
+            var hours = data.date.getHours();
+            var hoursArray = [
+                "twelve",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "ten",
+                "eleven",
+                "twelve"
+            ];
+            if (data.words.to.status){
+                data.words[hoursArray[(hours % 12) + 1]].status = true;
+            } else {
+                data.words[hoursArray[(hours % 12)]].status = true;
+            }
+        },
+        setMeridian: function(){
+            var hours = data.date.getHours();
+            if( (hours / 12) > 0 ){
+                data.words.pm.status = true;
+            } else {
+                data.words.am.status = true;
+            }
+        },
+        getWords: function(){
+            return data.words;
+        }
+    };
+
+    var view = {
+        init: function(){
+            var words = controller.getWords();
+            for (var word in words){
+                var container = document.getElementById('wordList');
+                var node = document.createElement('LI');
+                var textNode = document.createTextNode(words[word].text);
+                node.id = word;
+                if (words[word].status) node.className = "on";
+                node.appendChild(textNode);
+                container.appendChild(node);
+            }
+        },
+        render: function(){
+            var words = controller.getWords();
+            for (var word in words) {
+                var node = document.getElementById(word);
+                if(words[word]){
+                    node.className = "on";
+                } else {
+                    node.className = "";
+                }
+            }
+        }
+    };
+
+    controller.init();
+})();
